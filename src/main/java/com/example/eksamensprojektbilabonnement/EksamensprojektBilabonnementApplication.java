47e068366b91f63fe7cb74223ab948743f874f8c
Magnompower <143427171+Magnompower@users.noreply.github.com>
2024-05-03 10:56:55 +0200

    Bilabonnement påbegyndt

---
+++ src/main/java/com/example/eksamensprojektbilabonnement/EksamensprojektBilabonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojektbilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensprojektBilabonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensprojektBilabonnementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamensprojektbilabonnement/EksamensprojektBilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojektbilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensprojektBilabonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
