f582ee452ed8fa0f60dafd072b8131cc264a9ffd
Anders <andersravn94@gmail.com>
2024-05-08 11:34:55 +0200

    INVENTORY working very much yes yes - CCS also

--- src/main/java/com/example/eksamensprojektbilabonnement/controllers/InventoryController.java
+++ src/main/java/com/example/eksamensprojektbilabonnement/controllers/InventoryController.java
@@ -1,6 +1,5 @@
 package com.example.eksamensprojektbilabonnement.controllers;
 
-import com.example.eksamensprojektbilabonnement.models.inheritance.GasCar;
 import com.example.eksamensprojektbilabonnement.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.eksamensprojektbilabonnement.models.inheritance.Car;
 import com.example.eksamensprojektbilabonnement.services.InventoryService;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.List;
 
@@ -22,9 +22,9 @@ public class InventoryController {
 
     @GetMapping("/inventory")
     public String showInventory(Model model) {
-        List<Car> cars = inventoryService.getAllCars();
+        List<Car> cars = inventoryService.getAllCars(); // Assuming you have a method to retrieve all gas cars
         model.addAttribute("cars", cars);
-        return "home/inventory";
+        return "home/inventory"; // The name of your HTML template
     }
     @GetMapping ("/view_car")
     public String view_car(@RequestParam String carChassisNumber, Model model) {
