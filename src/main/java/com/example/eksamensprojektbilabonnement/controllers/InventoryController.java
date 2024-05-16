5c62219739de9db54f72a3fa79051b532894ed89
HasanRashid97 <hasanrashid97@gmail.com>
2024-05-16 15:26:25 +0200

    removed duplicate pending Cars option

--- src/main/java/com/example/eksamensprojektbilabonnement/controllers/InventoryController.java
+++ src/main/java/com/example/eksamensprojektbilabonnement/controllers/InventoryController.java
@@ -1,12 +1,10 @@
 package com.example.eksamensprojektbilabonnement.controllers;
 
-import com.example.eksamensprojektbilabonnement.services.CarService;
-import com.example.eksamensprojektbilabonnement.services.CustomerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import com.example.eksamensprojektbilabonnement.models.inheritance.Car;
 import com.example.eksamensprojektbilabonnement.services.InventoryService;
 import org.springframework.web.bind.annotation.ModelAttribute;
