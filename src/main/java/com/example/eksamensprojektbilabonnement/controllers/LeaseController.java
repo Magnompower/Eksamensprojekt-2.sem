08f347f6bd4a90f470fe6ea8f842a6add35d5fe2
sgtasswipe <otro0001@stud.kea.dk>
2024-05-21 13:03:33 +0200

    added unit tests

--- src/main/java/com/example/eksamensprojektbilabonnement/controllers/LeaseController.java
+++ src/main/java/com/example/eksamensprojektbilabonnement/controllers/LeaseController.java
@@ -1,10 +1,8 @@
 package com.example.eksamensprojektbilabonnement.controllers;
 
 import com.example.eksamensprojektbilabonnement.services.CarService;
-import com.example.eksamensprojektbilabonnement.services.CustomerService;
 import com.example.eksamensprojektbilabonnement.services.LeaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
